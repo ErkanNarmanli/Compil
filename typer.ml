590acf2aa40475d533a8fef209d4f4cc68d8fac7
Qwann <qwann@hotmail.fr>
2015-11-28 15:03:01 +0100

    ok

--- typer.ml
+++ typer.ml
@@ -8,5 +8,19 @@ type typ =
     | Tstring
     | Tnull
     | Tnothing
+    | Tvar of tvar
     | Tclasse of typ list
     | Tmethode of typ list * typ
+
+and tvar = {
+    id          : int ;
+    mutable def : typ option }
+
+module V = struct
+    type t  = tvar
+    let compare v1 v2   = Pervasives.compare v1.id v2.id
+    let equal   v1 v2   = v1.id = v2.id
+    let create          =   let r = ref 0
+                            in  fun () -> incr r;
+                                {id     = !r ;
+                                def     = None }
