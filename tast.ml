e5b668c4a42c0a2928237910f54f29012b6f6f31
Kerl13 <martin.pepin@netcourrier.com>
2015-11-29 19:57:49 +0100

    tast up to date

--- tast.ml
+++ tast.ml
@@ -1,11 +1,5 @@
 (* Arbre de syntaxe abstraite décoré de Mini-Scala*)
 
-open Ast
-
-(*ON DÉFINIT DES OBJETS QUI SERONT UTILES PLUS TARD*)
-type loc    = Lexing.position * Lexing.position (* début, fin *)
-type ident  = string
-
 type typ =
     | Tany
     | TanyVal
@@ -18,6 +12,7 @@ type typ =
     | Tnothing
     | Tmethode of typ list * typ
 
+
 (* Un map indexé par des chaines de caractères *)
 module Smap = Map.Make(String)
 
@@ -25,26 +20,35 @@ module Smap = Map.Make(String)
 type constr = string * typ
 
 (* Et un ensemble de contraintes *)
-module Cset = Set.Make(constr)
+module Cset = Set.Make(
+    struct 
+        let compare = Pervasives.compare
+        type t = constr
+    end
+)
+
+
+(*ON DÉFINIT DES OBJETS QUI SERONT UTILES PLUS TARD*)
+type loc    = Lexing.position * Lexing.position (* début, fin *)
+type ident  = string
 
 (* env (= environnement local) est un ensemble de classes,
  *  et un ensemble de de contraintes de type,    
  *  et une suite ordonée de déclarations de variables *)
-type context = classe Smap.t * Cset.t * var list
-let empty_context = Smap.empty * Cset.empty * []
+type context = tclasse Smap.t * Cset.t * tvar list
 
 (*ON DÉFINIT L'ARBRE À PROPREMENT PARLER*)
-type tfichier = {
+and tfichier = {
     tclasses    : tclasse list ;
     tmain       : tclasse_Main ;
     tf_loc      : loc ; 
     tf_env      : context}
 
 and tclasse = {
-    tc_name             : indent ;
-    ttype_class_params  : (tparam_type_classe list) option ; (*débile ?!*)
-    tparams             : (tparametre list) option ; (*débile ?!*)
-    tderiv              : (ttyp * texpr list option ) option ;
+    tc_name             : ident ;
+    ttype_class_params  : tparam_type_classe list;
+    tparams             : tparametre list ; 
+    tderiv              : (ttyp * texpr list) option ;
     tdecls              : tdecl list ;
     tc_loc              : loc ; 
     tc_env              : context ; }
@@ -59,8 +63,8 @@ and tvar = {
     tv_typ      : typ       ; }
 
 and tvarCont = 
-    | TVal  of indent * (ttyp option) * texpr
-    | TVar  of indent * (ttyp option) * texpr
+    | TVal  of ident * (ttyp option) * texpr
+    | TVar  of ident * (ttyp option) * texpr
 
 and tmethode = {
     tm_cont     : tmethodeCont  ;
@@ -68,7 +72,7 @@ and tmethode = {
                                    * nom de la methode, idéalement *)
     tm_typ      : typ           ; (* Ouais, hein ? Je sais plus, laissons dans
                                    * le doute *)
-    tm_env      : contex        ; }
+    tm_env      : context        ; }
 
 and tmethodeCont = 
     | TMbloc    of tmeth_bloc
@@ -81,7 +85,7 @@ and tmeth_bloc = {
     tmb_params          : tparametre list ;
     tbloc               : tbloc ; }
 
-and tmeh_expr = {
+and tmeth_expr = {
     tme_name            : ident ;
     tme_override        : bool ;
     tme_type_params     : (tparam_type list) option ;
@@ -104,25 +108,32 @@ and tparam_type = {
 
 and tparam_typeCont = ident * tparam_type_heritage option
 
+and tparam_type_classe = {
+    tptc_cont           : tparam_type_classeCont ;
+    tptc_loc            : loc }
+and tparam_type_classeCont =
+    | TPTCplus  of tparam_type
+    | TPTCmoins of tparam_type
+    | TPTCrien  of tparam_type
+
 and ttyp = {
     tt_name             : ident ;
     targs_type          : targuments_type ;
     tt_loc              : loc }
 
 and targuments_type = {
-    tat_
+    tat_cont            : targuments_typeCont ;
+    tat_col             : loc }
 
-(* On ne redéfinit pas param_type_classe *)
-(* On ne redéfinit pas typ *)
-(* On ne reféfinit pas arguments_type *)
+and targuments_typeCont = ttyp list
 
 and tclasse_Main = {
     tcM_cont    : tclasse_MainCont  ;
     tcM_loc     : loc               ; } (* idem juste la loc du mot clef
                                         * 'class Main', c'est mieux *)
-and tclasse_MainCont = tdec list ;
+and tclasse_MainCont = tdecl list
 
-and texr = {
+and texpr = {
     te_cont     : texprCont ;
     te_loc      : loc       ;
     te_typ      : typ       ; }
@@ -136,11 +147,11 @@ and texprCont =
     | TEbool     of bool
     | TEacc      of tacces
     | TEacc_exp      of tacces * texpr
-    | TEacc_typ_exp  of tacces * arguments_type * texpr list
-    | TEnew      of ident * arguments_type * texpr list
+    | TEacc_typ_exp  of tacces * targuments_type * texpr list
+    | TEnew      of ident * targuments_type * texpr list
     | TEneg      of texpr
     | TEmoins    of texpr
-    | TEbinop    of binop * texpr * texpr 
+    | TEbinop    of tbinop * texpr * texpr 
     | TEif       of texpr * texpr
     | TEifelse   of texpr * texpr * texpr
     | TEwhile    of texpr * texpr
@@ -151,10 +162,15 @@ and texprCont =
 and tbloc = tinstruction list (* osef de la loc d'une instruction, quid du type ? *)
 
 and tinstruction =
-    | TIvar     of var
+    | TIvar     of tvar
     | TIexpr    of texpr
 
-(* On en redéfinit pas binop *)
+and tbinop = {
+    tb_cont             : tbinopCont ;
+    tb_loc              : loc }
+
+and tbinopCont = EqRef | NeRef | Eq | Ne | Lt | Le | Gt | Ge | Add | Sub | Mul |
+Div | Mod | And | Or
 
 and tacces = {
     ta_cont     : taccesCont    ;
@@ -162,5 +178,5 @@ and tacces = {
     ta_typ      : typ           ; } (* On type, non ? *)
 
 and taccesCont = 
-    | TAident       of indent
-    | TAexpr_indent of texpr * indent
+    | TAident       of ident
+    | TAexpr_ident of texpr * ident
